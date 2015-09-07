315d9e57814c976f945bcfe67b497c280204af23
Juliano F. Ribeiro <jucafribeiro@gmail.com>
2015-09-06 21:01:16 -0300

    Melhoria nos métodos responsáveis pela impressão das jogadas válidas.

--- src/breakthrough_parte01/TestBreakthrough.java
+++ src/breakthrough_parte01/TestBreakthrough.java
@@ -8,18 +8,10 @@ public class TestBreakthrough {
 
     public static void main(String args[])
     {
-    	int[] j = new int[4];
+    	int[] movimento = new int[4];
+    	Scanner input = new Scanner(System.in);
         Breakthrough breakthrough = new Breakthrough();
-        ArrayList<Jogadas> lista = breakthrough.jogadasValidasBrancas();
-
-//        breakthrough.imprimeJogadasBrancas();
-//        System.out.print(lista);
-//        Scanner input = new Scanner(System.in);
-//        System.out.printf("%s", "Informe a posição de origem e de destino da jogada: ");
-//        Jogadas jogada = new Jogadas(j[0], j[1], j[2], j[3]);
-//        input.close();
-//        System.out.println("Pressione qualquer tecla para efetuar uma jogada e imprimir o novo tabuleiro");
-//        new java.util.Scanner(System.in).nextLine();
-
+        ArrayList<Jogadas> jogadasBrancas = breakthrough.jogadasValidasBrancas();
+        ArrayList<Jogadas> jogadasPretas = breakthrough.jogadasValidasPretas();
     }
 }
